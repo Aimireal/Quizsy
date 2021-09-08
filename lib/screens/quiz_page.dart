c270e3d03c1462b6da8b927fcf2b89a4cc94caee
Dylan <dylanhudson1998@gmail.com>
2021-09-08 21:03:08 +0100

    Added supporting functionality for beginning a quiz, finishing a quiz, checking results and errors

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -6,19 +6,158 @@ import 'package:quizapp/models/categories.dart';
 import 'package:quizapp/models/questions.dart';
 import 'package:quizapp/screens/quiz_finished.dart';
 
-class QuizPage extends StatefulWidget{
+
+class QuizPage extends StatefulWidget {
   final List<Question> questions;
   final Category category;
-  const QuizPage({Key? key, required this.questions, required this.category}) : super(key: key);
+  const QuizPage({Key? key, required this.questions, required this.category}): super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _QuizPageState();
+  _QuizPageState createState() => _QuizPageState();
 }
 
-class _QuizPageState extends State<QuizPage>{
+class _QuizPageState extends State<QuizPage> {
+  final TextStyle _questionStyle = TextStyle(
+      fontSize: 18.0, fontWeight: FontWeight.w500, color: Colors.white);
+
+  int _currentIndex = 0;
+  final Map<int, dynamic> _answers = {};
+  final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Question question = widget.questions[_currentIndex];
+    final List<dynamic> options = question.incorrectAnswers;
+    if (!options.contains(question.correctAnswer)) {
+      options.add(question.correctAnswer);
+      options.shuffle();
+    }
+
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        key: _key,
+        appBar: AppBar(
+          title: Text(widget.category.name),
+          elevation: 0,
+        ),
+        body: Stack(
+          children: <Widget>[
+            ClipPath(
+              clipper: WaveClipperTwo(),
+              child: Container(
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
+                height: 200,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white70,
+                        child: Text("${_currentIndex + 1}"),
+                      ),
+                      SizedBox(width: 16.0),
+                      Expanded(
+                        child: Text(
+                          HtmlUnescape().convert(
+                              widget.questions[_currentIndex].question),
+                          softWrap: true,
+                          style: MediaQuery.of(context).size.width > 800
+                              ? _questionStyle.copyWith(fontSize: 30.0)
+                              : _questionStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ...options.map((option) => RadioListTile<dynamic>(
+                              title: Text(HtmlUnescape().convert("$option"),
+                              style: MediaQuery.of(context).size.width > 800 ? 
+                                TextStyle(
+                                  fontSize: 30.0
+                                ) : null,
+                              ),
+                              groupValue: _answers[_currentIndex] ,
+                              value: option,
+                              onChanged: (value) {
+                                setState(() {
+                                  _answers[_currentIndex] = option;
+                                });
+                              },
+                            )),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      child: ElevatedButton(
+                        child: Text(
+                            _currentIndex == (widget.questions.length - 1)
+                                ? "Submit"
+                                : "Next", style: MediaQuery.of(context).size.width > 800
+                              ? TextStyle(fontSize: 30.0) : null,),
+                        onPressed: _nextSubmit,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _nextSubmit() {
+    if (_answers[_currentIndex] == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("You must select an answer to continue.")
+      ));
+      return;
+    }
+    if (_currentIndex < (widget.questions.length - 1)) {
+      setState(() {
+        _currentIndex++;
+      });
+    } else {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (_) => QuizFinishedPage(
+              questions: widget.questions, answers: _answers)));
+    }
+  }
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Are you sure you want to quit the quiz? All your progress will be lost."),
+        title: Text("Warning!"),
+        actions: <Widget>[
+          ElevatedButton(
+            child: Text("Yes"),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
+          ElevatedButton(
+            child: Text("No"),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+        ],
+      )
+    ) ==true;
   }
 }
